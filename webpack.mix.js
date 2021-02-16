8e5fde9f8d4f5972348f165d088eef93c2f6a0a8
haba-sensei <72942765+haba-sensei@users.noreply.github.com>
2021-02-16 01:28:33 -0500

    backend_haba
    plugins y demas conf web

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,11 +3,15 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').version()
 mix.js('resources/js/dash.js', 'public/dist/js').version()
+mix.js('resources/js/web.js', 'public/dist/js').version()
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
     ])
     .postCss('resources/css/dash.css', 'public/dist/css')
+    .postCss('resources/css/web.css', 'public/dist/css')
+    .postCss('resources/css/base_web.css', 'public/dist/css')
+    .postCss('resources/css/plugins.css', 'public/dist/plugins')
 
 .webpackConfig(require('./webpack.config'))
     .copyDirectory('resources/json', 'public/dist/json')
