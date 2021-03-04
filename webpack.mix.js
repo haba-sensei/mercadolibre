2647c6f068acf44cb58348fbe6f7d969f38c3785
haba-sensei <72942765+haba-sensei@users.noreply.github.com>
2021-03-04 03:15:47 -0500

    backend_haba
    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,19 +3,20 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').version()
 mix.js('resources/js/dash.js', 'public/dist/js').version()
-mix.js('resources/js/web.js', 'public/dist/js').version()
+mix.js('resources/javascripts/web.js', 'public/dist/js').version()
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
     ])
-    .postCss('resources/css/dash.css', 'public/dist/css')
-    .postCss('resources/css/base_web.css', 'public/dist/css')
+    .postCss('resources/css/dash.css', 'public/dist/css').version()
+    .postCss('resources/css/base_web.css', 'public/dist/css').version()
 
 
-.webpackConfig(require('./webpack.config'))
-    .copyDirectory('resources/json', 'public/dist/json')
-    .copyDirectory('resources/fonts', 'public/dist/fonts')
-    .copyDirectory('resources/images', 'public/dist/images')
+mix.webpackConfig(require('./webpack.config'))
+    // mix.copyDirectory('resources/json', 'public/dist/json')
+    // mix.copyDirectory('resources/fonts', 'public/dist/fonts')
+    // mix.copyDirectory('resources/images', 'public/dist/images')
+    // mix.copyDirectory('resources/plugins', 'public/dist/plugins')
     .browserSync({
         proxy: "mercadolibre.test",
         files: ["resources/**/*.*"],
